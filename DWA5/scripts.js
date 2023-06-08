7865102e0424c0ca5687720978ca1edc419d3f62
Tshwanelomosii <percymosii1@gmail.com>
2023-06-08 15:57:42 +0200

    created dwa6

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -1,122 +1,123 @@
-const form = document.querySelector("[data-form]");
-const result = document.querySelector("[data-result]");
+// const form = document.querySelector("[data-form]");
+// const result = document.querySelector("[data-result]");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const entries = new FormData(event.target);
+//   const { dividend, divider } = Object.fromEntries(entries);
 
-  const wholenumber = dividend / divider;
-
-  if(isNaN(dividend) || isNaN(divider)) {
-    document.body.innerText = " Something critical went wrong. Please reload the page.";
-    console.error("Invalid input provided. Program crashed");
-  }
+//   const wholenumber = dividend / divider;
+// // Check if either dividend or divider is not a number
+//   if(isNaN(dividend) || isNaN(divider)) {
+//     document.body.innerText = " Something critical went wrong. Please reload the page.";
+//     console.error("Invalid input provided. Program crashed");
+//   }
+//   // Check if either dividend or divider is empty
+//   else if( dividend === "" || divider === "") {
+//     result.innerText = " Division not performed. Both values are required in inputs. Try again "; 
+//   }
+//   // Check if either dividend or divider is negative
+//   else if (dividend < 0 || divider < 0) {
+//     result.innerText = "Division not performed. Invalid number provided. Try again"
+//   }
+//   // Perform division and display the result as a whole number
+//   else result.innerText = Math.floor(wholenumber)
   
-  else if( dividend === "" || divider === "") {
-    result.innerText = " Division not performed. Both values are required in inputs. Try again "; 
-  }
-  
-  else if (dividend < 0 || divider < 0) {
-    result.innerText = "Division not performed. Invalid number provided. Try again"
-  }
-  else result.innerText = Math.floor(wholenumber)
-  
-});
+// });
 
 
-// /**
-//   * 
-//   * @param {string} label 
-//   * @returns {HTMLElement}
-//   */
-// const getHtml = (label) => {
-//     const node = document.querySelector(`[data-${label}]`)
-//     if (!(node instanceof HTMLElement)){
-//         throw new Error(
-//             "[data-${label}] was not found in HTML"
-//         )
-//     } 
+/**
+  * 
+  * @param {string} label 
+  * @returns {HTMLElement}
+  */
+const getHtml = (label) => {
+    const node = document.querySelector(`[data-${label}]`)
+    if (!(node instanceof HTMLElement)){
+        throw new Error(
+            "[data-${label}] was not found in HTML"
+        )
+    } 
 
-//     return node
+    return node
     
-// }
-// const html = {
-//     mode:{
-//         display: getHtml('mode-display'),
-//         button: getHtml('mode-button'),
-//     },
+}
+const html = {
+    mode:{
+        display: getHtml('mode-display'),
+        button: getHtml('mode-button'),
+    },
 
-//     intensity:{
-//     display: getHtml('mode-display'),
-//     button: getHtml('mode-button'),
-//     },
-// }
+    intensity:{
+    display: getHtml('mode-display'),
+    button: getHtml('mode-button'),
+    },
+}
 
 
-// const toggleIntensity = () => {
+const toggleIntensity = () => {
 
-//     const lockFn = () => {
-//         if (locked) throw new error ('Already locked') 
-//         locked = true;
+    const lockFn = () => {
+        if (locked) throw new error ('Already locked') 
+        locked = true;
     
-//         return () => {
-//             locked =false
-//         }
-//     }
+        return () => {
+        data.locked =false
+        }
+    }
     
     
     
-//     const toggle = () => {
-//         const unlock = lockFn()
+    const toggle = () => {
+        const unlock = lockFn()
     
-//         setTimeout(() =>{
-//             locked = false
-//         },2000 )
-//     }
-//     const ChangeAmount = () => {
-//         // if (locked) throw new Error ('Mode is being changed')
-//         // locked = true 
-//         const unlock = lockFn()
+        setTimeout(() =>{
+            locked = false
+        },2000 )
+    }
+    const ChangeAmount = () => {
+        // if (locked) throw new Error ('Mode is being changed')
+        // locked = true 
+        const unlock = lockFn()
     
-//         unlock()
+        unlock()
     
-//         // locked = false 
-//     }
+        // locked = false 
+    }
     
-//     toggleMode()
-//     ChangeAmount()
+    toggleMode()
+    ChangeAmount()
 
-// }
-// const toggleMode = () => {
+}
+const toggleMode = () => {
 
-// }
-
-
-
-// const mode = document.querySelector(['data-mode'])
-// const intensity = document.querySelector(['data-intensity'])
-
-// mode.addEventListener('click',() =>{})
-// intensity.addEventListener('click',() =>{})
-
-// /**
-//  * @typedef {object} data
-//  * @prop {'low' |'high'} intensity
-//  * @prop {'wide'|'focus'} mode
-//  * @prop {'boolean'} locked
-//  * */
-
-// /**
-//  * @type {Data}
-//  */
-
-// const data = {
-//         intensity: 'low',
-//         mode: 'wide',
-//         locked: false,
-// }
+}
 
 
-// let locked = false
+
+const mode = document.querySelector(['data-mode'])
+const intensity = document.querySelector(['data-intensity'])
+
+mode.addEventListener('click',() =>{})
+intensity.addEventListener('click',() =>{})
+
+/**
+ * @typedef {object} data
+ * @prop {'low' |'high'} intensity
+ * @prop {'wide'|'focus'} mode
+ * @prop {'boolean'} locked
+ * */
+
+/**
+ * @type {Data}
+ */
+
+const data = {
+        intensity: 'low',
+        mode: 'wide',
+        locked: false,
+}
+
+
+let locked = false
 
